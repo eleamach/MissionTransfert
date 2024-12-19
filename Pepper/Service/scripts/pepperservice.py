f10ccf30fb3677c22146194f674773769b1498ca
Elea Machillot <elea.machillot@tparlem04.cpe.lan>
2024-12-19 17:11:45 +0100

    code propre

--- Pepper/Service/scripts/pepperservice.py
+++ Pepper/Service/scripts/pepperservice.py
@@ -8,47 +8,38 @@ class PepperService(object):
     APP_ID = "com.elea.pepperservice"
 
     def __init__(self, qiapp):
-            self.qiapp = qiapp
-            self.session = qiapp.session
-            self.tts = self.session.service("ALTextToSpeech")
-            self.tts.setLanguage("French")
-            self.audio_player = self.session.service("ALAudioPlayer")
-            self.logger = logging.getLogger(self.APP_ID)
-            logging.basicConfig(level=logging.INFO)
-            self.motion = self.session.service("ALMotion")
-            self.posture = self.session.service("ALRobotPosture")
-            self.animation_player = self.session.service("ALAnimationPlayer")
-            self.tts.setParameter("speed", 110)
-            self.tts.setParameter("pitchShift", 1.1)
-            self.memory = self.session.service("ALMemory")
-            self.active_subscriptions = set()
-            self.head_touch_subscription = self.memory.subscriber("TouchChanged")
-            self.head_touch_subscription.signal.connect(self.on_touch_changed)
-            self.tablet = self.session.service("ALTabletService")
-            self.tablet.loadApplication("elea")
-            self.tablet.showWebview()
+        self.qiapp = qiapp
+        self.session = qiapp.session
+        self.tts = self.session.service("ALTextToSpeech")
+        self.tts.setLanguage("French")
+        self.audio_player = self.session.service("ALAudioPlayer")
+        self.logger = logging.getLogger(self.APP_ID)
+        logging.basicConfig(level=logging.INFO)
+        self.motion = self.session.service("ALMotion")
+        self.posture = self.session.service("ALRobotPosture")
+        self.animation_player = self.session.service("ALAnimationPlayer")
+        self.tts.setParameter("speed", 110)
+        self.tts.setParameter("pitchShift", 1.1)
+        self.memory = self.session.service("ALMemory")
+        self.active_subscriptions = set()
+        self.head_touch_subscription = self.memory.subscriber("TouchChanged")
+        self.head_touch_subscription.signal.connect(self.on_touch_changed)
+        self.tablet = self.session.service("ALTabletService")
+        self.tablet.loadApplication("elea")
+        self.tablet.showWebview()
         
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def play_animation(self, animation_name):
         try:
             self.animation_player.run(animation_name) 
         except Exception as e:
             self.logger.error("Erreur lors du lancement de l'animation : {}".format(e))
             
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String, qi.Int32])
     def raise_event(self, event_name, value):
-        """
-        Cette méthode permet de lever un événement dans ALMemory.
-        :param event_name: Nom de l'événement.
-        :param value: Valeur associée à l'événement.
-        """
         try:
             self.memory.raiseEvent(event_name, value)
-            self.logger.info("Événement {} levé avec la valeur {}".format(event_name, value))
         except Exception as e:
             self.logger.error("Erreur lors de la levée de l'événement {}: {}".format(event_name, e))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String, qi.String, qi.String, qi.String, qi.String])
     def speak_n_animate(self, id, datakey, animation, professors, maitre ):
         try:
             if professors:
@@ -64,43 +55,36 @@ class PepperService(object):
             self.play_animation(animation) 
             self.tts.say(data[datakey][id])
         except Exception as e:
-            print("Erreur lors de la parole ou de l'animation: {}".format(e))
+            self.logger.error("Erreur lors de la parole ou de l'animation: {}".format(e))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def play_music(self, file):        
         try:
             file_id = self.audio_player.loadFile(file)
             self.audio_player.play(file_id)
             self.running_audio_ids = [file_id]
         except Exception as e:
-            self.logger.error("Error while playing music : {}".format(e))
+            self.logger.error("Erreur lors de la lecture de la musique : {}".format(e))
     
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def subscribe(self, speech_recognition, module_name):
         if module_name not in self.active_subscriptions:
             try:
                 speech_recognition.subscribe(module_name)
                 self.active_subscriptions.add(module_name)
-                self.logger.info("Abonnement réussi à {}".format(module_name))
             except Exception as e:
                 self.logger.error("Erreur lors de l'abonnement à {} : {}".format(module_name, e))
         else:
             self.logger.warning("{} est déjà abonné.".format(module_name))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def unsubscribe(self, speech_recognition, module_name):
         if module_name in self.active_subscriptions:
             try:
                 speech_recognition.unsubscribe(module_name)
                 self.active_subscriptions.remove(module_name)
-                self.logger.info("Désabonnement réussi de {}".format(module_name))
             except Exception as e:
                 self.logger.error("Erreur lors du désabonnement de {} : {}".format(module_name, e))
         else:
                 self.logger.warning("Tentative de désabonnement de {}, mais il n'était pas abonné.".format(module_name))
-
     
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def on_touch_changed(self, data):
         try:
             for touch in data:
@@ -112,13 +96,11 @@ class PepperService(object):
                         speech_recognition = self.session.service("ALSpeechRecognition")
                         vocabulary = ["oui", "non"]
                         speech_recognition.pause(True)
-                        self.logger.info("Définition du vocabulaire : {}".format(vocabulary))
                         speech_recognition.setVocabulary(vocabulary, False)
                         speech_recognition.pause(False)
                         memory = self.session.service("ALMemory")
                         word_recognized_subscriber = memory.subscriber("WordRecognized")
                         word_recognized_subscriber.signal.connect(self.on_word_recognized)
-                        self.logger.info("Signal connecté à on_word_recognized.")
                         self.subscribe(speech_recognition, "IndiceDialog")
                         time.sleep(10) 
                     except Exception as e:
@@ -127,18 +109,17 @@ class PepperService(object):
         except Exception as e:
             self.logger.error("Erreur lors du traitement des données tactiles : {}".format(e))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def on_word_recognized(self, data):
         try:
             word = data[0]
             confidence = data[1]            
             if confidence > 0.5:
                 if word == "oui":
-                    self.tts.say("A quel atelier etes-vous ? Si vous avez un doute sur le nom, regardez l'étiquette vers votre atelier") 
+                    self.tts.say("A quel atelier êtes-vous ? Si vous avez un doute sur le nom, regardez l'étiquette vers votre atelier") 
                     try:
                         speech_recognition = self.session.service("ALSpeechRecognition")
                         speech_recognition.pause(True)
-                        workshops = ["Simon", "Masterminde", "Texte"] #TODO: modifier et ajouter les ateliers
+                        workshops = ["Simon", "Masterminde", "Texte"] 
                         speech_recognition.setVocabulary(workshops, False)
                         speech_recognition.pause(False)
                         workshop_recognition_subscriber = self.memory.subscriber("WordRecognized")
@@ -152,7 +133,6 @@ class PepperService(object):
         except Exception as e:
             self.logger.error("Erreur lors du traitement de la reconnaissance vocale : {}".format(e))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def on_workshop_recognized(self, data):
         try:
             workshop = data[0]  
@@ -162,7 +142,6 @@ class PepperService(object):
                 self.tts.say("Vous avez choisi l'atelier {}. Voulez-vous le premier indice, le second indice ou la solution ?".format(workshop))
                 self.selected_workshop = workshop  
                 try:
-                    speech_recognition = self.session.service("ALSpeechRecognition")
                     speech_recognition.pause(True)
                     options = ["premier", "second", "solution"]  
                     speech_recognition.setVocabulary(options, False)
@@ -177,7 +156,6 @@ class PepperService(object):
         except Exception as e:
             self.logger.error("Erreur lors de la reconnaissance du nom de l'atelier : {}".format(e))
     
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String])
     def on_hint_recognized(self, data):
         try:
             if data[0] == "premier":
@@ -186,7 +164,6 @@ class PepperService(object):
                 self.hint = "1"
             elif data[0] == "solution":
                 self.hint = "2" 
-            print("Hint {}".format(self.hint))
             speech_recognition = self.session.service("ALSpeechRecognition")
             if data[1] > 0.5:  
                 if self.hint in ["0", "1", "2"]: 
@@ -198,7 +175,6 @@ class PepperService(object):
         except Exception as e:
             self.logger.error("Erreur lors de la reconnaissance du choix de l'indice : {}".format(e))
 
-    @qi.bind(returnType=qi.Void, paramsType=[qi.String]) 
     def get_json(self, file):
         try:
             with open(file, "r") as f:
@@ -208,7 +184,6 @@ class PepperService(object):
             self.logger.error("Erreur lors de la lecture du fichier JSON : {}".format(e))
             return {}
 
-    @qi.bind(returnType=qi.Void, paramsType=[])
     def stop(self):
         try:
             self.cleanup_subscriptions()
@@ -221,10 +196,10 @@ class PepperService(object):
         for subscription in list(self.active_subscriptions):
             try:
                 speech_recognition.unsubscribe(subscription)
-                self.logger.info("Désabonnement réussi de {}".format(subscription))
             except Exception as e:
                 self.logger.error("Erreur lors du désabonnement de {}".format(subscription))
         self.active_subscriptions.clear()
+
 
 if __name__ == "__main__":
     import stk.runner
